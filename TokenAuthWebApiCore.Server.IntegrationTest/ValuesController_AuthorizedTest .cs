a097d8da86d22db9fdfa7f85655c5f5409abee17
TOCANE TECHNOLOGIES <tocane.technologies@gmail.com>
2017-05-25 23:40:23 +0200

    rename

--- TokenAuthWebApiCore.Server.IntegrationTest/ValuesController_AuthorizedTest .cs
+++ TokenAuthWebApiCore.Server.IntegrationTest/ValuesController_AuthorizedTest .cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using TokenAuthWebApiCore.Server.IntegrationTest.Setup;
 using TokenAuthWebApiCore.Server.Models;
 using Xunit;
+using System;
+
 namespace TokenAuthWebApiCore.Server.IntegrationTest
 {
 	public class ValuesController_AuthorizedTest  : IClassFixture<TestFixture<Startup>>
@@ -25,6 +27,24 @@ namespace TokenAuthWebApiCore.Server.IntegrationTest
 		public async Task WhenAuthenticatedUser_MakeRequestRequest_Return_Ok(string method,string obj =null, int? id =null)
 		{
 			// Arrange
+			var jwToken = await getJwToken();
+			string token =$"bearer {jwToken.token}";
+						
+			string stringData = JsonConvert.SerializeObject(obj);
+			var contentData = new StringContent(stringData, Encoding.UTF8, "application/json");
+
+			var request = new HttpRequestMessage(new HttpMethod(method), $"/api/values/{id}");
+			request.Content = contentData;
+			request.Headers.Add("Authorization", token);
+			// Act
+			var response = await Client.SendAsync(request);
+
+			// Assert
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		}
+
+		private async Task<JwToken> getJwToken()
+		{
 			var loginViewModel = new LoginViewModel
 			{
 				Email = "simpleuser@yopmail.com",
@@ -32,24 +52,12 @@ namespace TokenAuthWebApiCore.Server.IntegrationTest
 			};
 			string loginViewModelData = JsonConvert.SerializeObject(loginViewModel);
 			var contentData = new StringContent(loginViewModelData, Encoding.UTF8, "application/json");
-			
+
 			var response = await Client.PostAsync($"/api/auth/token", contentData);
 			response.EnsureSuccessStatusCode();
 			var jwToken = JsonConvert.DeserializeObject<JwToken>(
 				await response.Content.ReadAsStringAsync());
-			string token =$"bearer {jwToken.token}";
-						
-			string stringData = JsonConvert.SerializeObject(obj);
-			contentData = new StringContent(stringData, Encoding.UTF8, "application/json");
-
-			var request = new HttpRequestMessage(new HttpMethod(method), $"/api/values/{id}");
-			request.Content = contentData;
-			request.Headers.Add("Authorization", token);
-			// Act
-			response = await Client.SendAsync(request);
-
-			// Assert
-			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+			return jwToken;
 		}
 	}
 }
